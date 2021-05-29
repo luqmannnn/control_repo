6a574790dbe38162800202b66fcffdd80efac19c
luqmannnn <luqmannurhakimt@gmail.com>
2021-05-29 16:42:10 +0800

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  docker::node {'web.puppet.vm':}
-  docker::node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
