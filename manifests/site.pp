70c455f49382f7bf8686f6c754da8f3a9dd1b412
luqmannnn <luqmannurhakimt@gmail.com>
2021-05-29 16:22:57 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,11 @@
-
+node default {
+  file {'/root/README':
+    ensure => file,
+    content => 'Puppet Configuration file',
+    owner => 'root',
+  }
+  
+  file {'/root/README':
+    owner => root,
+  }
+}
